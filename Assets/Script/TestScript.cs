a5c07bc9ebda6b8884678ce427d6c9449092af1c
Keizo Nagamine <morison-b-keizo@hotmail.co.jp>
2013-07-25 15:24:55 +0900

    スタート時のログ出力追加

--- Assets/Script/TestScript.cs
+++ Assets/Script/TestScript.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		Debug.Log("[LOG]TestScript");
 	}
 	
 	// Update is called once per frame
